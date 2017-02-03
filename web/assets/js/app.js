530d78b0517947a6c3af53bcaafe915afffe99ae
Andrey Kuzmenko <andrey.kuzmenko@yamalspg.ru>
2017-02-03 16:36:30 +0300

    styles update

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -24,6 +24,8 @@
 
   function resizeBroadcast() {
 
+  	return; // зачем это?? IE не одобряет
+
     var timesRun = 0;
     var interval = setInterval(function(){
       timesRun += 1;
@@ -114,40 +116,7 @@ $(document).ready(function($){
 	});
 
   init();
-
-    /*var demo = new StickyElement($('#sticky'));*/
 });
-
-/*var StickyElement = function(node){
-    var doc = $(document),
-        fixed = false,
-        anchor = node.find('.sticky-anchor'),
-        content = node.find('.sticky-content');
-
-    var onScroll = function(e){
-        var docTop = doc.scrollTop(),
-            anchorTop = anchor.offset().top;
-
-        console.log('scroll', docTop, anchorTop);
-        if(docTop > anchorTop){
-            if(!fixed){
-                anchor.height(content.outerHeight());
-                content.addClass('fixed');
-                fixed = true;
-            }
-        }  else   {
-            if(fixed){
-                anchor.height(0);
-                content.removeClass('fixed');
-                fixed = false;
-            }
-        }
-    };
-
-    $(window).on('scroll', onScroll);
-};*/
-
-
 
 /****
 * CARDS ACTIONS
