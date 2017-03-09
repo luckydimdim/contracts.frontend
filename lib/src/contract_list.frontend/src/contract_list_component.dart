c2aeb69ac810eaf1bf8e579b312f2daa7964cc59
Dmitriy <Dmitriy.Litovchenko@yamalspg.ru>
2017-03-09 10:23:20 +0300

    CMAS-52 Выполнить доработки Frontend'a для реализации функционала просмотра карточки договора

--- lib/src/contract_list.frontend/src/contract_list_component.dart
+++ lib/src/contract_list.frontend/src/contract_list_component.dart
@@ -31,16 +31,6 @@ class ContractListComponent implements OnInit, AfterViewInit {
   @override
   void ngOnInit() {
     breadcrumbInit();
-
-    var table = querySelector('[table-click]') as TableElement;
-    table.rows.forEach((TableRowElement row) {
-      row.onClick.listen((MouseEvent e) {
-        var currentRow = e.currentTarget as TableRowElement;
-        String link = currentRow.getAttribute('data-href');
-
-        _router.parent.navigateByUrl(link);
-      });
-    });
   }
 
   @override
