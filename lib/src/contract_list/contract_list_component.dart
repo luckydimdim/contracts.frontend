88e1ab7718de8482d7a2991be346d977c66d9c8c
Andrey Kuzmenko <andrey.kuzmenko@yamalspg.ru>
2017-06-15 13:36:28 +0300

    loading spin

--- lib/src/contract_list/contract_list_component.dart
+++ lib/src/contract_list/contract_list_component.dart
@@ -4,18 +4,25 @@ import 'package:angular2/core.dart';
 import 'package:angular2/router.dart';
 
 import 'package:auth/auth_service.dart';
-import 'package:angular_utils/cm_format_money_pipe.dart';
-import 'package:angular_utils/cm_format_currency_pipe.dart';
-
+import 'package:angular_utils/pipes.dart';
+import 'package:grid/grid.dart';
 import '../contract/general/contract_general_model.dart';
 import '../contract/general/contract_general_service.dart';
 
 @Component(
     selector: 'contract-list',
     templateUrl: 'contract_list_component.html',
-    directives: const [RouterLink],
-    pipes: const [CmFormatMoneyPipe, CmFormatCurrencyPipe])
-class ContractListComponent implements AfterViewInit, OnInit {
+    directives: const [
+      RouterLink,
+      GridComponent,
+      GridTemplateDirective,
+      ColumnComponent,
+    ],
+    pipes: const [
+      CmFormatMoneyPipe,
+      CmFormatCurrencyPipe
+    ])
+class ContractListComponent implements OnInit {
   final Router _router;
   final ContractGeneralService _service;
   final AuthorizationService _authorizationService;
@@ -23,38 +30,26 @@ class ContractListComponent implements AfterViewInit, OnInit {
 
   bool readOnly = true;
 
-  List<Map> contracts = new List<Map>();
+  DataSource dataSource;
 
   ContractListComponent(
       this._router, this._service, this._authorizationService);
 
   @override
-  ngOnInit() {
+  ngOnInit() async {
     if (_authorizationService.isInRole(Role.Customer)) readOnly = false;
-  }
 
-  @override
-  ngAfterViewInit() async {
     List<ContractGeneralModel> contractsList = await _service.getContracts();
 
+    List<Map> contracts = new List<Map>();
     for (ContractGeneralModel contract in contractsList) {
       contracts.add(contract.toMap());
     }
+
+    dataSource = new DataSource(data: contracts)..primaryField = 'id';
   }
 
   Future createContract() async {
-    /*var newContractModel = new ContractGeneralModel();
-
-    String contractId = await _service.general.createContract(newContractModel);
-
-    _service.writeEnabled = true;
-    _router.parent.navigate([
-      'Contract',
-      {'id': contractId}
-    ]);
-
-    return null;*/
-
     _router.parent.navigate([
       'ContractCreate',
     ]);
